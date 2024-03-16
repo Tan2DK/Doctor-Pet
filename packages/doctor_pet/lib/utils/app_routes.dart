3feb70ee28f60cc6f32b78d244a1e045cd219f8d
tranduy1505 <hanamichi3101@gmail.com>
2024-03-16 19:49:05 +0700

    UI Doctor Schduletime

--- packages/doctor_pet/lib/utils/app_routes.dart
+++ packages/doctor_pet/lib/utils/app_routes.dart
@@ -10,6 +10,8 @@ import 'package:doctor_pet/views/pet/pet_binding.dart';
 
 import '../views/schedule/schedule_binding.dart';
 import '../views/schedule/schedule_view.dart';
+import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_binding.dart';
+import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_view.dart';
 
 class AppRoutes {
   AppRoutes._();
@@ -35,6 +37,11 @@ class AppRoutes {
     //   page: () => const ScheduleView(),
     //   binding: ScheduleBinding(),
     // ),
+    GetPage(
+      name: RoutesName.scheduleTime,
+      page: () => const DoctorScheduletimeView(),
+      binding: DoctorScheduletimeBinding(),
+    ),
   ];
 }
 
@@ -43,7 +50,7 @@ class RoutesName {
   static const String doctor = '/doctor';
   static const String pet = '/pet';
   static const String schedule = '/schedule';
-  static const String scheduleTime = '/scheduleTime';
+  static const String scheduleTime = '/schedule-time';
   static const String nestedNavDoctor = 'nested_navigation_doctor';
   static const String nestedNavPet = 'nested_navigation_pet';
 }
--- packages/doctor_pet/lib/views/doctor/doctor_scheduletime/widgets/dialog_time_slot.dart
+++ packages/doctor_pet/lib/views/doctor/doctor_scheduletime/widgets/dialog_time_slot.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_controller.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_controller.dart';
 
-class DialogCustom extends StatelessWidget {
-  DialogCustom({Key? key}) : super(key: key);
+// class DialogCustom extends StatelessWidget {
+//   DialogCustom({Key? key}) : super(key: key);
 
-  final DoctorScheduletimeController controller =
-      Get.find<DoctorScheduletimeController>();
+//   final DoctorScheduletimeController controller =
+//       Get.find<DoctorScheduletimeController>();
 
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Edit Time Slots'),
-      content: SingleChildScrollView(
-        child: ElevatedButton(
-          onPressed: controller.onShowScheduleSlot,
-          child: const Text('Edit'),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return AlertDialog(
+//       title: const Text('Edit Time Slots'),
+//       content: SingleChildScrollView(
+//         child: ElevatedButton(
+//           onPressed: controller.onShowScheduleSlot,
+//           child: const Text('Edit'),
+//         ),
+//       ),
+//     );
+//   }
+// }
