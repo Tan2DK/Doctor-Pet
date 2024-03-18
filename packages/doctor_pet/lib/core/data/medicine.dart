630577a664b6ee0e68145a938947583972adb85f
khoi <tathuynhanhkhoi@gmail.com>
2024-03-18 17:17:33 +0700

    add

--- packages/doctor_pet/lib/core/data/medicine.dart
+++ packages/doctor_pet/lib/core/data/medicine.dart
@@ -1,13 +1,39 @@
-class Medicine{
-  String idMedicine ;
+class Medicine {
+  String idMedicine;
   String nameMedicine;
-  String companyMedicineName  ;
+  String companyMedicineName;
   String quantity;
   DateTime importDate;
   DateTime expirationDate;
   String price;
- 
-  
 
-  Medicine({required this.idMedicine,required this.nameMedicine,required this.companyMedicineName,required this.quantity,required this.importDate,required this.expirationDate,required this.price});
+  Medicine({
+    required this.idMedicine,
+    required this.nameMedicine,
+    required this.companyMedicineName,
+    required this.quantity,
+    required this.importDate,
+    required this.expirationDate,
+    required this.price,
+  });
+
+  Medicine copyWith({
+    String? idMedicine,
+    String? nameMedicine,
+    String? companyMedicineName,
+    String? quantity,
+    DateTime? importDate,
+    DateTime? expirationDate,
+    String? price,
+  }) {
+    return Medicine(
+      idMedicine: idMedicine ?? this.idMedicine,
+      nameMedicine: nameMedicine ?? this.nameMedicine,
+      companyMedicineName: companyMedicineName ?? this.companyMedicineName,
+      quantity: quantity ?? this.quantity,
+      importDate: importDate ?? this.importDate,
+      expirationDate: expirationDate ?? this.expirationDate,
+      price: price ?? this.price,
+    );
+  }
 }
