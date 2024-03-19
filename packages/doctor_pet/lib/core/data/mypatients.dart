04d22878eb8dd9f64f47b5b9b5854175f76de175
tranduy1505 <hanamichi3101@gmail.com>
2024-03-19 17:55:37 +0700

    UI Doctor my patients update details  to review

--- packages/doctor_pet/lib/core/data/mypatients.dart
+++ packages/doctor_pet/lib/core/data/mypatients.dart
@@ -1,17 +1,25 @@
+import 'package:doctor_pet/core/data/my_patients_details.dart';
+
 class Patients {
   String customerId;
-  String name;
-  String phoneNumber;
-  DateTime birthday;
-  String address;
+  String petName;
+  String petAge;
+  String petGender;
+  String petColor;
+  String petTypeId;
+  String petSpecies;
   DateTime dateBuyMedicine;
+  PatientsDetails details;
 
   Patients({
     required this.customerId,
-    required this.name,
-    required this.phoneNumber,
-    required this.birthday,
-    required this.address,
+    required this.petName,
+    required this.petAge,
+    required this.petGender,
+    required this.petColor,
+    required this.petTypeId,
+    required this.petSpecies,
     required this.dateBuyMedicine,
+    required this.details,
   });
 }
--- packages/doctor_pet/lib/views/doctor/doctor_mypatients/my_patients_view.dart
+++ packages/doctor_pet/lib/views/doctor/doctor_mypatients/my_patients_view.dart
@@ -11,65 +11,80 @@ class MyPatientsView extends GetView<MyPatientsController> {
     final controller = Get.find<MyPatientsController>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Patients View'),
-      ),
       body: Container(
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 254, 234, 234),
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 254, 234, 234),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: const InputDecoration(
+              padding: const EdgeInsets.all(8.0),
+              child: const TextField(
+                decoration: InputDecoration(
                   hintText: 'Search...',
                   prefixIcon: Icon(Icons.search),
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
-            SizedBox(height: 10),
-            const Center(
-              child: Text(
-                'View My Patients',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+            // const SizedBox(height: 10),
+            // const Center(
+            //   child: Text(
+            //     'View My Patients',
+            //     style: TextStyle(
+            //       fontSize: 24,
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            // ),
+            const SizedBox(height: 10),
+            const Row(
+              children: [
+                SizedBox(width: 70),
+                Text(
+                  'View My Patients',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
+              ],
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             Expanded(
-              child: SingleChildScrollView(
-                child: DataTable(
-                  columnSpacing: 10,
-                  horizontalMargin: 10,
-                  dataRowHeight: 50,
-                  columns: const [
-                    DataColumn(label: Text('ID Customer')),
-                    DataColumn(label: Text('Name')),
-                    DataColumn(label: Text('Phone Number')),
-                    DataColumn(label: Text('Birthday')),
-                    DataColumn(label: Text('Address')),
-                    DataColumn(label: Text('Date Buy')),
-                  ],
-                  rows: controller.myPatients.value.map((patient) {
-                    return DataRow(cells: [
-                      DataCell(Text(patient.customerId)),
-                      DataCell(Text(patient.name)),
-                      DataCell(Text(patient.phoneNumber)),
-                      DataCell(Text(
-                          DateFormat('yyyy-MM-dd').format(patient.birthday))),
-                      DataCell(Text(patient.address)),
-                      DataCell(Text(DateFormat('yyyy-MM-dd')
-                          .format(patient.dateBuyMedicine))),
-                    ]);
-                  }).toList(),
-                ),
-              ),
+              child: Obx(() => SingleChildScrollView(
+                    child: DataTable(
+                      columnSpacing: 50,
+                      horizontalMargin: 10,
+                      dataRowHeight: 50,
+                      columns: const [
+                        DataColumn(label: Text('ID Customer')),
+                        DataColumn(label: Text('Pet Name')),
+                        DataColumn(label: Text('Pet Age')),
+                        DataColumn(label: Text('Pet Species')),
+                        DataColumn(label: Text('Pet Color')),
+                        DataColumn(label: Text('Date Buy Medicine')),
+                        DataColumn(label: Text('Details')),
+                      ],
+                      rows: controller.myPatients.value.map((patient) {
+                        return DataRow(cells: [
+                          DataCell(Text(patient.customerId)),
+                          DataCell(Text(patient.petName)),
+                          DataCell(Text(patient.petAge)),
+                          DataCell(Text(patient.petSpecies)),
+                          DataCell(Text(patient.petColor)),
+                          DataCell(Text(DateFormat('yyyy-MM-dd')
+                              .format(patient.dateBuyMedicine))),
+                          DataCell(IconButton(
+                            icon: const Icon(Icons.account_balance),
+                            onPressed: () =>
+                                controller.showDetails(patient.details),
+                          )),
+                        ]);
+                      }).toList(),
+                    ),
+                  )),
             ),
           ],
         ),
