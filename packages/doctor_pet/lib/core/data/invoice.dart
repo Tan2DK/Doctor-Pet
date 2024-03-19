2f1d69eef54164fcf4a18bf0ef0e1bb267315688
tranduy1505 <hanamichi3101@gmail.com>
2024-03-20 00:50:52 +0700

    UI Invoice fix to review

--- packages/doctor_pet/lib/core/data/invoice.dart
+++ packages/doctor_pet/lib/core/data/invoice.dart
@@ -2,15 +2,21 @@ class Invoice {
   String idMedicine;
   String nameMedicine;
   String companyMedicineName;
+  DateTime date;
   DateTime dateMedicine;
   String expirationdate;
+  String noteDoctor;
+  String totals;
   String price;
 
   Invoice(
       {required this.idMedicine,
       required this.nameMedicine,
       required this.companyMedicineName,
+      required this.date,
       required this.dateMedicine,
       required this.expirationdate,
-      required this.price});
+      required this.noteDoctor,
+      required this.price,
+      required this.totals});
 }
--- packages/doctor_pet/lib/data/data_mock/doctor_invoice.dart
+++ packages/doctor_pet/lib/data/data_mock/doctor_invoice.dart
@@ -1,22 +1,27 @@
-// File: doctor_medicine.dart
 import 'package:doctor_pet/core/data/invoice.dart';
 
 final List<Invoice> mockInvoice = [
   Invoice(
+    date: DateTime(2018, 01, 10),
     dateMedicine: DateTime(2018, 01, 10),
     idMedicine: '05',
     nameMedicine: 'Ibuprofen',
     companyMedicineName: 'Pharmaco',
     expirationdate: '12/2026',
-    price: '8',
+    noteDoctor: 'Take 8 per day',
+    price: '10', // Giả định về giá
+    totals: '80', // Tổng giả định, có thể bạn cần tính dựa trên số lượng và giá
   ),
   Invoice(
-    dateMedicine: DateTime(2019, 02, 15),
+    date: DateTime(2019, 02, 15),
+    dateMedicine: DateTime(2018, 01, 10),
     idMedicine: '06',
     nameMedicine: 'Acetaminophen',
     companyMedicineName: 'Medicorp',
     expirationdate: '11/2024',
-    price: '3',
+    noteDoctor: 'Take 3 per day',
+    price: '15',
+    totals: '45',
   ),
-  // Thêm các mục medicine khác tương tự
+  // Thêm các mục Invoice khác tương tự
 ];
