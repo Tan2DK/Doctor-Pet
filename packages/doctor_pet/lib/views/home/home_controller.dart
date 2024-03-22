b7c8bcbd670a251ebaa71e694e0145c83e3d1e99
tranduy1505 <hanamichi3101@gmail.com>
2024-03-23 01:14:23 +0700

    UI doctor schedule Time to review

--- packages/doctor_pet/lib/views/home/home_controller.dart
+++ packages/doctor_pet/lib/views/home/home_controller.dart
@@ -56,6 +56,7 @@ class HomeController extends GetxController {
   List<Widget> listScreen() {
     return [
       const NestedNavigationPet(),
+      const NestedNavigationScheduleTimeDoctor(),
     ];
   }
 }
--- packages/doctor_pet/lib/views/home/nested_navigation/nested_navigation_doctor_time.dart
+++ packages/doctor_pet/lib/views/home/nested_navigation/nested_navigation_doctor_time.dart
@@ -1,27 +1,25 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-// import '../../../utils/app_routes.dart';
-// import '../../pet/pet_binding.dart';
-// import '../../pet/pet_view.dart';
-// import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_binding.dart';
-// import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_view.dart';
+import '../../../utils/app_routes.dart';
+import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_binding.dart';
+import 'package:doctor_pet/views/doctor/doctor_scheduletime/doctor_scheduletime_view.dart';
 
-// class NestedNavigationTimeDoctor extends StatelessWidget {
-//   const NestedNavigationTimeDoctor({super.key});
+class NestedNavigationScheduleTimeDoctor extends StatelessWidget {
+  const NestedNavigationScheduleTimeDoctor({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Navigator(
-//       key: Get.nestedKey(RoutesName.nestedNavPet),
-//       initialRoute: "/",
-//       onGenerateRoute: (settings) {
-//         return GetPageRoute(
-//           settings: settings,
-//           binding: DoctorScheduletimeBinding(),
-//           page: () => const DoctorScheduletimeView(),
-//         );
-//       },
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: Get.nestedKey(RoutesName.scheduleTime),
+      initialRoute: "/",
+      onGenerateRoute: (settings) {
+        return GetPageRoute(
+          settings: settings,
+          binding: DoctorScheduletimeBinding(),
+          page: () => const DoctorScheduletimeView(),
+        );
+      },
+    );
+  }
+}
