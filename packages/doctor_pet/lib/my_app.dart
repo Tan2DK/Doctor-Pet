fa487d8ec116b5a69d91aa75d56ddd8d3f122f30
khoi <tathuynhanhkhoi@gmail.com>
2024-03-24 17:38:50 +0700

    fix

--- packages/doctor_pet/lib/my_app.dart
+++ packages/doctor_pet/lib/my_app.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'core/theme/app_theme.dart';
 import 'language/language.dart';
 import 'utils/app_constant.dart';
@@ -38,7 +39,17 @@ Future<void> runMyApp() async {
       getPages: AppRoutes.mainRoutes,
       theme: AppThemes.lightTheme,
       translations: Language(),
-      builder: EasyLoading.init(),
+      builder: EasyLoading.init(
+        builder: (context, child) => ResponsiveBreakpoints.builder(
+          child: child!,
+          breakpoints: [
+            const Breakpoint(start: 0, end: 450, name: MOBILE),
+            const Breakpoint(start: 451, end: 800, name: TABLET),
+            const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+            const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+          ],
+        ),
+      ),
     ),
   );
 }
